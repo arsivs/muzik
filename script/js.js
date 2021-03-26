39ccee2ebcd354a82f93999925b8a74a4d2d3120
arsivs <50826181+arsivs@users.noreply.github.com>
2021-03-26 19:47:29 +0300

    Add files via upload

---
+++ script/js.js
@@ -0,0 +1,30 @@
+﻿(function (arr) {
+
+})
+
+function yaz(items){
+	var yaz = document.getElementById("yaz");
+	items.forEach(function(item, i) {
+		var liste = yaz.querySelector('.liste').cloneNode(true);
+		if(i == 0)
+			yaz.innerText = ""; //yaz.appendChild(liste.cloneNode(true));
+		liste.querySelector('.sol b').innerText = item.title;
+		liste.querySelector('.sag .sure').innerText = item.duration;
+		liste.querySelector('.sol span i').innerText = item.popularity;
+		liste.querySelector('.sol span u').innerText = item.date;
+		liste.querySelector('.sag a').href = item.url;
+		yaz.appendChild(liste);
+	});
+}
+
+function include(url) {
+	var script  = document.createElement('script');
+	script.src  = url;
+	script.type = 'text/javascript';
+	script.defer = true;
+	document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+include("script/yt_search.js");
+include("script/temizle.js");
+include("script/plugins.js");
